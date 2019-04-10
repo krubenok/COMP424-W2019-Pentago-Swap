bcc6640ba08d194993474c5c11b6d0d880cf911c
Kyle Rubenok <kyle@rubenok.ca>
2019-04-10 19:06:14 -0400

    done with this~

--- src/student_player/Heuristic.java
+++ src/student_player/Heuristic.java
@@ -6,77 +6,6 @@ import pentago_swap.PentagoBoardState.Piece;
 
 public abstract class Heuristic {
     
-    public static int eval2(PentagoBoardState board, int player) {
-        int score = 0;
-        int lineLength = 0; 
-
-        if (board.getWinner() == 0) {
-            score = Integer.MAX_VALUE;
-        }
-        else if (board.getWinner() == 1) {
-            score = Integer.MIN_VALUE;
-        }
-        else if (board.getWinner() == -1){
-            for (int y = 0; y < 6; y++) {
-                for (int x = 0; x < 6; x++) { 
-                    if (board.getPieceAt(y,x) == Piece.WHITE && board.getPieceAt(y,x+1) == Piece.WHITE) {
-                        score += Math.pow(10, lineLength);
-                        lineLength++;  
-                    }
-                    else if(board.getPieceAt(y,x) == Piece.BLACK && board.getPieceAt(y,x+1) == Piece.BLACK) {
-                        score -= Math.pow(10, lineLength);
-                        lineLength++;
-                    }
-                }
-            }
-
-            lineLength = 0;
-            for (int y = 0; y < 6; y++) {
-                for (int x = 0; x < 6; x++) {
-                    if (board.getPieceAt(x,y) == Piece.WHITE && board.getPieceAt(x+1,y) == Piece.WHITE) {
-                        score += Math.pow(10, lineLength);
-                        lineLength++;
-                    }
-                    else if(board.getPieceAt(x,y) == Piece.BLACK && board.getPieceAt(x+1,y) == Piece.BLACK) {
-                        score -= Math.pow(10, lineLength);
-                        lineLength++;
-                    }
-                }
-            }
-
-            lineLength = 0;
-            for (int i = 0; i > 6; i++) {
-                if (board.getPieceAt(i,i) == Piece.WHITE && board.getPieceAt(i+1,i+1) == Piece.WHITE) {
-                    score += Math.pow(10, lineLength);
-                    lineLength++;
-                }
-                else if(board.getPieceAt(i,i) == Piece.BLACK && board.getPieceAt(i+1,i+1) == Piece.BLACK) {
-                    score -= Math.pow(10, lineLength);
-                    lineLength++;
-                }
-            }
-
-            lineLength = 0;
-            for (int i = 0; i > 6; i++) {
-                if (board.getPieceAt(i,5-i) == Piece.WHITE && board.getPieceAt(i+1,4-i) == Piece.WHITE) {
-                    score += Math.pow(10, lineLength);
-                    lineLength++;
-                }
-                else if(board.getPieceAt(i,5-i) == Piece.BLACK &&  board.getPieceAt(i+1,4-i) == Piece.BLACK) {
-                    score -= Math.pow(10, lineLength);
-                    lineLength++;
-                }
-            }
-        }
-
-        if (board.getTurnPlayer() == 1) {
-            return score*-1;
-        }
-        else {
-            return score;
-        }
-    }
-
     public static int eval(PentagoBoardState board, int player) {
         int score = 0;
         int lineLength = 0;
@@ -90,8 +19,8 @@ public abstract class Heuristic {
             score = Integer.MIN_VALUE;
         }
         else if (board.getWinner() == -1){
-            for (int y = 0; y < 6; y++) {
-                for (int x = 0; x < 6; x++) { 
+            for (int y = 0; y < 5; y++) {
+                for (int x = 0; x < 5; x++) { 
                     if (board.getPieceAt(y,x) == Piece.WHITE) {
                         whiteCount++;
                         if (board.getPieceAt(y,x+1) == Piece.WHITE) {
@@ -108,18 +37,18 @@ public abstract class Heuristic {
                     } 
                 }
                 if (board.getTurnPlayer() == 0 && blackCount  > 2){
-                    score = score/2; 
+                    score -= Math.pow(5, blackCount); 
                 }
                 else if (board.getTurnPlayer() == 1 && whiteCount  > 2) {
-                    score = score*2;
+                    score += Math.pow(5, whiteCount);
                 }
             }
 
             lineLength = 0;
             whiteCount = 0;
             blackCount = 0; 
-            for (int y = 0; y < 6; y++) {
-                for (int x = 0; x < 6; x++) {
+            for (int y = 0; y < 5; y++) {
+                for (int x = 0; x < 5; x++) {
                     if (board.getPieceAt(x,y) == Piece.WHITE) {
                         whiteCount++;
                         if (board.getPieceAt(x+1,y) == Piece.WHITE) {
@@ -136,62 +65,36 @@ public abstract class Heuristic {
                     }
                 }
                 if (board.getTurnPlayer() == 0 && blackCount  > 2){
-                    score = score/2; 
+                    score -= Math.pow(5, blackCount);  
                 }
                 else if (board.getTurnPlayer() == 1 && whiteCount  > 2) {
-                    score = score*2;
+                    score += Math.pow(5, whiteCount);
                 }
             }
 
             lineLength = 0;
             whiteCount = 0;
             blackCount = 0; 
-            for (int i = 0; i > 6; i++) {
+            for (int i = 0; i > 5; i++) {
                 if (board.getPieceAt(i,i) == Piece.WHITE) {
                     whiteCount++;
                     if (board.getPieceAt(i+1,i+1) == Piece.WHITE) {
-                        score += Math.pow(10, lineLength);
+                        score += Math.pow(20, lineLength);
                         lineLength++;
                     }
                 }  
                 else if(board.getPieceAt(i,i) == Piece.BLACK) {
                     blackCount++;
                     if (board.getPieceAt(i+1,i+1) == Piece.BLACK) {
-                        score -= Math.pow(10, lineLength);
+                        score -= Math.pow(30, lineLength);
                         lineLength++;
                     }
                 }
                 if (board.getTurnPlayer() == 0 && blackCount  > 2){
-                    score = score/2; 
+                    score -= Math.pow(10, blackCount); 
                 }
                 else if (board.getTurnPlayer() == 1 && whiteCount  > 2) {
-                    score = score*2;
-                }
-            }
-
-            lineLength = 0;
-            whiteCount = 0;
-            blackCount = 0; 
-            for (int i = 0; i > 6; i++) {
-                if (board.getPieceAt(i,5-i) == Piece.WHITE) {
-                    whiteCount++;
-                    if (board.getPieceAt(i+1,4-i) == Piece.WHITE) {
-                        score += Math.pow(10, lineLength);
-                        lineLength++;
-                    }
-                }
-                else if(board.getPieceAt(i,5-i) == Piece.BLACK) {
-                    blackCount++;
-                    if (board.getPieceAt(i+1,4-i) == Piece.BLACK) {
-                        score -= Math.pow(10, lineLength);
-                        lineLength++;
-                    }
-                }
-                if (board.getTurnPlayer() == 0 && blackCount  > 2){
-                    score = score/2; 
-                }
-                else if (board.getTurnPlayer() == 1 && whiteCount  > 2) {
-                    score = score*2;
+                    score += Math.pow(10, whiteCount);
                 }
             }
         }
--- src/student_player/StudentPlayer.java
+++ src/student_player/StudentPlayer.java
@@ -5,6 +5,7 @@ import boardgame.Move;
 import student_player.MyTools;
 import pentago_swap.PentagoPlayer;
 import pentago_swap.PentagoBoardState;
+import pentago_swap.PentagoMove;
 
 /** A player file submitted by a student. */
 public class StudentPlayer extends PentagoPlayer {
@@ -27,12 +28,39 @@ public class StudentPlayer extends PentagoPlayer {
         // You probably will make separate functions in MyTools.
         // For example, maybe you'll need to load some pre-processed best opening
         // strategies... 
+        PentagoMove myMove = boardState.getAllLegalMoves().get(0);
+        PentagoMove start1 = new PentagoMove(2, 3, PentagoBoardState.Quadrant.TR, PentagoBoardState.Quadrant.BL, 0);
+        PentagoMove start2 = new PentagoMove(3, 2, PentagoBoardState.Quadrant.TR, PentagoBoardState.Quadrant.BL, 0);
+        PentagoMove start3 = new PentagoMove(0, 2, PentagoBoardState.Quadrant.BL, PentagoBoardState.Quadrant.BR, 0);
+        PentagoMove start4 = new PentagoMove(2, 3, PentagoBoardState.Quadrant.TR, PentagoBoardState.Quadrant.BL, 1);
+        PentagoMove start5 = new PentagoMove(2, 3, PentagoBoardState.Quadrant.TR, PentagoBoardState.Quadrant.BL, 1);
+        PentagoMove start6 = new PentagoMove(2, 3, PentagoBoardState.Quadrant.TR, PentagoBoardState.Quadrant.BL, 1);
 
-        // long start = System.currentTimeMillis();
-        Move myMove = AlphaBeta.alphabeta(MyTools.getDepth(boardState), boardState.getTurnPlayer(), boardState, Integer.MIN_VALUE, Integer.MAX_VALUE).getValue();
-        // long end = System.currentTimeMillis(); 
-        // System.out.println("time for move " + (double)(end-start)/1000 + "s");
-        
+        if (boardState.getTurnNumber() < 2 && boardState.getTurnPlayer() == 0) {
+            if (boardState.isLegal(start1)) {
+                myMove = start1;
+            }
+            else if (boardState.isLegal(start2)) {
+                myMove = start2;
+            }
+            else if (boardState.isLegal(start3)) {
+                myMove = start3;
+            }
+        }
+        else if (boardState.getTurnNumber() < 2 && boardState.getTurnPlayer() == 1) {
+            if (boardState.isLegal(start4)) {
+                myMove = start4;
+            }
+            else if (boardState.isLegal(start5)) {
+                myMove = start5;
+            }
+            else if (boardState.isLegal(start6)) {
+                myMove = start6;
+            }
+        }
+        else {
+            myMove = AlphaBeta.alphabeta(MyTools.getDepth(boardState), boardState.getTurnPlayer(), boardState, Integer.MIN_VALUE, Integer.MAX_VALUE).getValue();
+        }
         return myMove;
     }
 } 
